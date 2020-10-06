4a55a84146775e2f82c265f41d4eba53c05af145
speed <speed@DESKTOP-6D55CHV.attlocal.net>
2020-10-05 22:58:00 -0700

    Version two with all functions given purpose and completed

--- cse360assignment02/src/cse360assignment02/AddingMachine.java
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -7,50 +7,59 @@ package cse360assignment02;
 public class AddingMachine {
 	/*
 	 * The integer total value for the adding Machine
+	 * Stores the calculations for the toString method in calculations
 	 */
 	
 	private int total;
+	private String calculations;
 	
 	/*
 	 * Adding Machine Constructor
 	 * Initializes total to 0
+	 * Initializes calculations to "0 "
 	 */
 	public AddingMachine() {
 		total = 0; //not needed - included for clarity
+		calculations = "0 ";
 	}
 	
 	/*
 	 * Method that returns total
 	 */
 	public int getTotal() {
-		return 0;
+		return total;
 	}
 	
 	/*
 	 * Adds an the value to the total
+	 * and update calculations
 	 */
 	public void add(int value) {
-		//to be implemented
+		total = total + value;
+		calculations = calculations + "+ " + value + " ";
 	}
 	
 	/*
 	 * Subtracts value from the total
+	 * and updates calculations
 	 */
 	public void subtract(int value) {
-		//to be implemented
+		total = total - value;
+		calculations = calculations + "- " + value + " ";
 	}
 	
 	/*
-	 * Returns the total value formatted as a string
+	 * Returns the memory formatted as a string
 	 */
 	public String toString() {
-		return ""; //to be implemented
+		return calculations; //to be implemented
 	}
 	
 	/*
-	 * clears the total
+	 * clears the memory by resetting both calculations and total
 	 */
 	public void clear() {
-		//to be implemented
+		calculations = "0 ";
+		total = 0;
 	}
 }
