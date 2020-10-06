5dfea6502ec702d91e82690a77eb6b56de02098d
speed <speed@DESKTOP-6D55CHV.attlocal.net>
2020-10-05 22:42:56 -0700

    Initial cse360 Assignment02

---
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,56 @@
+package cse360assignment02;
+
+/**
+ * An Adding Machine class that uses functions
+ * to add values and can clear
+ */
+public class AddingMachine {
+	/*
+	 * The integer total value for the adding Machine
+	 */
+	
+	private int total;
+	
+	/*
+	 * Adding Machine Constructor
+	 * Initializes total to 0
+	 */
+	public AddingMachine() {
+		total = 0; //not needed - included for clarity
+	}
+	
+	/*
+	 * Method that returns total
+	 */
+	public int getTotal() {
+		return 0;
+	}
+	
+	/*
+	 * Adds an the value to the total
+	 */
+	public void add(int value) {
+		//to be implemented
+	}
+	
+	/*
+	 * Subtracts value from the total
+	 */
+	public void subtract(int value) {
+		//to be implemented
+	}
+	
+	/*
+	 * Returns the total value formatted as a string
+	 */
+	public String toString() {
+		return ""; //to be implemented
+	}
+	
+	/*
+	 * clears the total
+	 */
+	public void clear() {
+		//to be implemented
+	}
+}
